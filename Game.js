63d2012aeeaa95ca08b8dc14adc47f51c36b461d
dregh1 <rakotonirinacharlesandrea@gmail.com>
2025-08-17 02:37:16 +0300

    place point at the closest intersection of click placement

--- Game.js
+++ Game.js
@@ -11,11 +11,13 @@ class Game {
         ];
         this.currentPlayerIndex = 0;
         this.points = []; // tous les points placés
+        this.intersections = []; // stocker toutes les intersections
         this.init();
     }
 
     init() {
         this.grid.draw();
+        this.generateIntersections();
         this.canvas.addEventListener('click', this.handleClick.bind(this));
         this.renderCurrentPlayer();
     }
@@ -29,22 +31,45 @@ class Game {
         this.renderCurrentPlayer();
     }
 
+    generateIntersections() {
+        // Génère toutes les intersections de la grille
+        for (let x = 0; x <= this.size.width; x += this.unite) {
+            for (let y = 0; y <= this.size.height; y += this.unite) {
+                this.intersections.push({x, y});
+            }
+        }
+    }
+
+    getClosestGridPoint(x, y) {
+        let minDist = Infinity;
+        let closestPoint = null;
+        for (const point of this.intersections) {
+            const dist = Math.hypot(point.x - x, point.y - y);
+            if (dist < minDist) {
+                minDist = dist;
+                closestPoint = point;
+            }
+        }
+        return closestPoint;
+    }
+
     handleClick(event) {
         const rect = this.canvas.getBoundingClientRect();
-        const x = event.clientX - rect.left;
-        const y = event.clientY - rect.top;
-        console.log(x,y);
-        if (this.grid.isOnGrid(x, y)) {
-            // Vérifier si le point est déjà pris
-            if (this.isPointAvailable(x, y)) {
-                const point = new Point(x, y, this.currentPlayer.name);
-                this.points.push(point);
-                this.currentPlayer.addPoint(point);
-                point.draw(this.ctx, this.currentPlayer.color);
-                this.switchPlayer();
-            } else {
-                alert('Point déjà pris');
-            }
+        const clickX = event.clientX - rect.left;
+        const clickY = event.clientY - rect.top;
+
+        // Obtenir la position la plus proche sur la grille
+        const closestPoint = this.getClosestGridPoint(clickX, clickY);
+
+        // Vérifier si le point est disponible
+        if (this.isPointAvailable(closestPoint.x, closestPoint.y)) {
+            const point = new Point(closestPoint.x, closestPoint.y, this.currentPlayer.name);
+            this.points.push(point);
+            this.currentPlayer.addPoint(point);
+            point.draw(this.ctx, this.currentPlayer.color);
+            this.switchPlayer();
+        } else {
+            alert('Point déjà pris');
         }
     }
 
@@ -53,7 +78,6 @@ class Game {
     }
 
     renderCurrentPlayer() {
-        // Affiche le nom du joueur actuel ou autre indication
         document.getElementById('currentPlayer').innerText = "Tour de : " + this.currentPlayer.name;
     }
 }
